ff89bc7fa3e68a68a6bae85067bdcf1747e40623
aaronburt <email@aaronburt.co.uk>
2022-06-15 02:11:49 +0100

    1.0.1

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const multer  = require('multer');
+const upload = require('./multer');
 const app = express();
-const upload = require('./src/multer')
+const { server_port, server_name, default_protocol, default_download_path } = process.env;
 
-const { server_port, server_name, server_scheme } = process.env;
+app.use(helmet());
+app.disable('etag');
+app.use(`/${default_download_path}`, express.static('src/storage-pool'));
 
-app.use('/download', express.static('src/storage-pool'));
+app.post('/upload', function (req, res) {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.sendStatus(400);
+        } else if (err) {
+            return res.sendStatus(400);
+        }
 
-app.post('/upload', upload.single('file'), (req, res) => { 
-    const wd = req.file.destination.split('/');
-    const last = wd[wd.length - 1];
-    return res.json({ "filename": `${server_scheme}://${server_name}/download/${last}/${req.file.filename}` }) 
+        const { filename } = req.file;
+        const protocol = req.protocol || default_protocol;
+        const host_name = req.headers.host || server_name;
+    
+        const wrkd = req.file.destination.replaceAll('\\', '/').split('/');
+        const last = wrkd[wrkd.length - 1];
+    
+        return res.json({ 
+            "filename": `${protocol}://${host_name}/${default_download_path}/${last}/${filename}`
+        }); 
+    });
 });
+
+app.all('*', (req, res) => { return res.sendStatus(404) })
 
 const listener = app.listen(server_port, () => { console.log(`[Express] ${listener.address().port}`) });
--- multer.js
+++ multer.js
@@ -1,20 +1,19 @@
-const { mkdirSync } = require('fs');
 const multer  = require('multer');
 const path = require('path');
+const { mkdirSync } = require('fs');
 
-const upload = multer({
+const upload = multer({    
+    limits: { fileSize: 25000000 },
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-
-            const currentPath = path.join(__dirname, 'storage-pool', `${Date.now()}`);
+            const currentPath = path.join(__dirname, 'src', 'storage-pool', `${Date.now()}`);
             mkdirSync(currentPath);
-            
             cb(null, path.join(currentPath))
         },
         filename: (req, file, cb) => {
             cb(null, file.originalname)
-        }
+        },
     })
-});
+}).single('file');
 
 module.exports = upload;
